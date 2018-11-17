e76b0a81a4808321b6db464573618f88d8a91d49
KingJeremy <kingjeremy2211@hotmail.com>
2018-11-16 23:32:57 -0600

    Updates

---
+++ precache-manifest.fb4859d9371b7c7e5cf66fb4db6d28c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72ef504fe05d29fd2637",
+    "url": "/Clickety-Click-Game/static/js/runtime~main.72ef504f.js"
+  },
+  {
+    "revision": "e1dafac1f517d47c294a",
+    "url": "/Clickety-Click-Game/static/js/main.e1dafac1.chunk.js"
+  },
+  {
+    "revision": "c5d36d2725223677ea1d",
+    "url": "/Clickety-Click-Game/static/js/1.c5d36d27.chunk.js"
+  },
+  {
+    "revision": "e1dafac1f517d47c294a",
+    "url": "/Clickety-Click-Game/static/css/main.4dd2f2bc.chunk.css"
+  },
+  {
+    "revision": "85b67fdede9e4c92390a5840bc984048",
+    "url": "/Clickety-Click-Game/index.html"
+  }
+];
